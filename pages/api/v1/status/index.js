cf756a65e1fec385e1562377612ca40607874767
João Henrique Santos Costa <94435966+joaohenriquecosta@users.noreply.github.com>
2026-01-09 05:08:26 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+console.log("antes da função status");
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log("Resultado da query:", result.rows);
   response.status(200).json({ mensagem: "teste status ok" });
 }
 
--- tests/unit/calculadora.test.js
+++
@@ -1,9 +0,0 @@
-const calculadora = require("../../models/calculadora.js");
-
-test("2+2 = 4", () => {
-  expect(calculadora.somar(2, 2)).toBe(4);
-});
-
-test("2+3 = 5", () => {
-  expect(calculadora.somar(2, 3)).toBe(5);
-});
