ccfc64f603527549e7c5f4751d130d76ed143e30
João Henrique Costa <joaohenrique_costa@icloud.com>
2026-03-01 10:29:31 -0300

    feat: 'implement `InternalServerError` and `database.js` and `api/v1/status` errors handling'

---
+++ infra/errors.js
@@ -0,0 +1,22 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno do servidor ocorreu.", { cause });
+    this.name = "InternalServerError";
+    this.action = "Se o problema persistir, entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      status_code: this.statusCode,
+      message: this.message,
+      action: this.action,
+      cause: {
+        name: this.cause.name,
+        message: this.cause.message,
+        code: this.cause.code,
+      },
+    };
+  }
+}
