939843621764f1d4cf228062219e6d02f31432cb
João Henrique Costa <joaohenrique_costa@icloud.com>
2026-03-07 15:24:31 -0300

    refactor(api): create `status` model with `getSystemStatus` and use it in `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
-import { query } from "infra/database";
 import { exceptionHandlers } from "infra/controller";
 import { createRouter } from "next-connect";
+import { getSystemStatus } from "models/status";
 
 const router = createRouter();
 
@@ -11,35 +11,6 @@ export default router.handler({
 });
 
 async function getHandler(request, response) {
-  const updatedAt = new Date().toISOString();
-
-  const dbVersionResult = await query("SHOW server_version;");
-  const dbVersionValue = dbVersionResult.rows[0].server_version;
-
-  const dbMaxConnectionsResult = await query("SHOW max_connections;");
-  const dbMaxConnectionsValue = parseInt(
-    dbMaxConnectionsResult.rows[0].max_connections,
-  );
-
-  const dbName = process.env.POSTGRES_DB;
-  const dbOpenedConnectionsResult = await query({
-    text: `SELECT COUNT(*) AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
-    values: [dbName],
-  });
-  const dbOpenedConnectionsValue = parseInt(
-    dbOpenedConnectionsResult.rows[0].opened_connections,
-  );
-
-  const responseBody = {
-    updated_at: updatedAt,
-    dependencies: {
-      db: {
-        version: dbVersionValue,
-        max_connections: dbMaxConnectionsValue,
-        opened_connections: dbOpenedConnectionsValue,
-      },
-    },
-  };
-
-  return response.status(200).json(responseBody);
+  const dbStatus = await getSystemStatus();
+  return response.status(200).json(dbStatus);
 }
