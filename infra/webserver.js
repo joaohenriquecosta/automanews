9c202d49d0d5e51c440e66a7f3e76adb48116e81
João Henrique Costa <joaohenrique_costa@icloud.com>
2026-05-01 17:13:26 -0300

    fix(CI): add local origin support for CI environment

--- infra/webserver.js
+++ infra/webserver.js
@@ -5,6 +5,10 @@ function getOrigin() {
     return `https://${process.env.VERCEL_URL}`;
   }
 
+  if (process.env.CI) {
+    return "http://127.0.0.1:3000";
+  }
+
   return process.env.NODE_ENV === "production"
     ? "https://automanews.com.br"
     : "http://localhost:3000";
